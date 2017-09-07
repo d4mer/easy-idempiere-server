84602764ddde817504935186f363689421e8f751
D4mer <d4mer@d4mations.com>
2017-09-07 18:23:52 +0200

    moved

--- idempiere_server_setup.sh
+++ idempiere_server_setup.sh
@@ -203,6 +203,8 @@ EOF
 echo ""
 echo "The informaton was saved in '$1' file."
 echo ""
+wget -q http://www.webmin.com/jcameron-key.asc -O- | sudo apt-key add -
+sudo apt-get update && install webmin
 break
 
 }
@@ -216,8 +218,7 @@ sudo apt-get install openssh-client openssh-server landscape-common nmap p7zip-f
 while true; do
   read -p "Would you like to install Webmin? [y/n]: " yn
   case $yn in
-    [Yy]* ) installWebmin $fileSource wget -q http://www.webmin.com/jcameron-key.asc -O- | sudo apt-key add -
-sudo apt-get update && install webmin --force-yes;;
+    [Yy]* ) installWebmin $fileSource ;;
     [Nn]* ) break;;
         * ) echo "Please enter y or n.";;
   esac
